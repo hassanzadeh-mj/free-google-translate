f3c75f3d3d464557fc640d9e4e15a727da302793
m.hassanzadeh <m.hassanzadeh@netbar.ir>
2025-05-26 04:15:21 +0200

    chore: move eslint config to cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+}; 
